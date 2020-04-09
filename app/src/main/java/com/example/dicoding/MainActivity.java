2264466b53478200eadf7846b3244c7809df105b
DimasTM <dimastanuji@gmail.com>
2020-04-09 12:44:53 +0700

    first commit

--- app/src/main/java/com/example/dicoding/MainActivity.java
+++ app/src/main/java/com/example/dicoding/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.dicoding;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -9,93 +10,34 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private EditText edtWidth, edtHeight, edtLength;
-    private Button btnCalculate;
-    private TextView tvResult;
-    private static final String STATE_RESULT = "state_result";
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putString(STATE_RESULT, tvResult.getText().toString());
-    }
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        edtWidth = findViewById(R.id.edt_width);
-        edtHeight = findViewById(R.id.edt_height);
-        edtLength = findViewById(R.id.edt_length);
-        btnCalculate = findViewById(R.id.btn_calculate);
-        tvResult = findViewById(R.id.tv_result);
+        Button btnMoveActivity = findViewById(R.id.btn_move_activity);
+        btnMoveActivity.setOnClickListener(this);
 
-        btnCalculate.setOnClickListener(this);
-
-        if (savedInstanceState != null) {
-            String result = savedInstanceState.getString(STATE_RESULT);
-            tvResult.setText(result);
-        }
+        Button btnMoveWithDataActivity = findViewById(R.id.btn_move_activity_data);
+        btnMoveWithDataActivity.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.btn_calculate) {
-            String inputLength = edtLength.getText().toString().trim();
-            String inputWidth = edtWidth.getText().toString().trim();
-            String inputHeight = edtHeight.getText().toString().trim();
+        switch (v.getId()) {
+            case R.id.btn_move_activity:
+                Intent moveIntent = new Intent(MainActivity.this, MoveActivity.class);
+                startActivity(moveIntent);
+                break;
 
-            boolean isEmptyFields = false;
-            boolean isInvalidDouble = false;
-
-            if (TextUtils.isEmpty(inputLength)) {
-                isEmptyFields = true;
-                edtLength.setError("Field ini tidak boleh kosong");
-            }
-
-            if (TextUtils.isEmpty(inputWidth)) {
-                isEmptyFields = true;
-                edtWidth.setError("Field ini tidak boleh kosong");
-            }
-
-            if (TextUtils.isEmpty(inputHeight)) {
-                isEmptyFields = true;
-                edtHeight.setError("Field ini tidak boleh kosong");
-            }
-
-            Double length = toDouble(inputLength);
-            Double width = toDouble(inputWidth);
-            Double height = toDouble(inputHeight);
-
-            if (length == null) {
-                isInvalidDouble = true;
-                edtLength.setError("Field ini harus berupa nomer yang valid");
-            }
-
-            if (width == null) {
-                isInvalidDouble = true;
-                edtWidth.setError("Field ini harus berupa nomer yang valid");
-            }
-
-            if (height == null) {
-                isInvalidDouble = true;
-                edtHeight.setError("Field ini harus berupa nomer yang valid");
-            }
-
-            if (!isEmptyFields && !isInvalidDouble) {
-                double volume = length * width * height;
-                tvResult.setText(String.valueOf(volume));
-            }
-        }
-    }
-
-    private Double toDouble(String str) {
-        try {
-            return Double.valueOf(str);
-        } catch (NumberFormatException e) {
-            return null;
+            case R.id.btn_move_activity_data:
+                Intent moveWithDataIntent = new Intent(MainActivity.this, MoveWithDataActivity.class);
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "DicodingAcademy Boy");
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 5);
+                startActivity(moveWithDataIntent);
+                break;
         }
     }
 }
